135976c643c53a8a42b48a286182de3a562c1569
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-07-23 20:56:13 +0530

    home added

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget{
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome>{
+  @override
+  Widget build(BuildContext buildContext){
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,47 @@
 //Mindula Dilthushan
 //Ax-Address-Book
 import 'package:flutter/material.dart';
+import 'package:ax_address_book/home.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Ax Address Book',
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: const Text('Ax Address Book'),
+  //       ),
+  //       body: const Center(
+  //         child: Text('This is Address Book Application'),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+// }
+
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ax Address Book',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ax Address Book'),
+  Widget build(BuildContext buildContext){
+    return MyHome(
+      seconds:2,
+      title:Text(
+        'Ax Address Book',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 35,
+          color: Color.white
         ),
-        body: const Center(
-          child: Text('This is Address Book Application'),
-        ),
-      ),
+      )
     );
   }
 }
+
+
 
 
